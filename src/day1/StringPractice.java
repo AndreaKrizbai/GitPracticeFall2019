103749401cff710bc2d9af82d4f3d9f70e9ce14f
AndreaKrizbai <krizbai.andrea@gmail.com>
2020-02-29 15:25:13 -0500

    modified StringPractice class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,5 +3,7 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        String str = "Jave is fun!";
+        System.out.println(str);
     }
 }
